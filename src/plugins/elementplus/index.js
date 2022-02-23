e948d2d5f1f973576e6cdeb7cd469c202ff1eccf
findnr <504875043@qq.com>
2022-02-23 11:00:36 +0800

    middify tailwindcss element-puls 01 02

--- src/plugins/elementplus/index.js
+++ src/plugins/elementplus/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 程英明
  * @Date: 2022-01-14 13:45:11
- * @LastEditTime: 2022-01-14 13:46:10
+ * @LastEditTime: 2022-02-23 10:59:35
  * @LastEditors: 程英明
  * @Description: 
  * @FilePath: \vue-element-plus-temp\src\plugins\elementplus\index.js
--- src/views/index/home/test2.vue
+++ src/views/index/home/test2.vue
@@ -1,23 +1,20 @@
 <!--
  * @Author: 程英明
  * @Date: 2022-01-17 15:28:13
- * @LastEditTime: 2022-01-17 15:28:28
+ * @LastEditTime: 2022-02-23 10:59:16
  * @LastEditors: 程英明
  * @Description: 
  * @FilePath: \vue-element-plus-temp\src\views\index\home\test2.vue
  * QQ:504875043@qq.com
 -->
 <template>
-  <div class='index'>
-这是home里面的test2
-  </div>
+  <div class="index">这是home里面的test2</div>
 </template>
 
 <script setup>
 </script>
 
 <style scoped lang="scss">
-
-.index{}
-
+.index {
+}
 </style>
