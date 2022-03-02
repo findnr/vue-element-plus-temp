1868f954aa30796183d882ca49fb107fec1e65ee
findnr <504875043@qq.com>
2022-03-02 10:49:24 +0800

    test page

--- src/views/admin/home/test.vue
+++ src/views/admin/home/test.vue
@@ -1,14 +1,13 @@
 <!--
  * @Author: your name
  * @Date: 2022-02-26 22:33:18
- * @LastEditTime: 2022-03-02 09:44:30
+ * @LastEditTime: 2022-03-02 10:49:18
  * @LastEditors: 程英明
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \vue-element-plus-temp\src\views\admin\home\test.vue
 -->
 <template>
   <div class="index">admin-home-test</div>
-  <el-button type="primary">Primary</el-button>
 </template>
 
 <script setup>
