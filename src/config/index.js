01ea0aa847ac46a456a2fe49bcad61afae2d5bbe
findnr <504875043@qq.com>
2022-03-09 11:42:09 +0800

    test

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 程英明
  * @Date: 2021-11-25 10:16:47
- * @LastEditTime: 2022-03-09 10:55:51
+ * @LastEditTime: 2022-03-09 11:41:52
  * @LastEditors: 程英明
  * @Description:
  * @FilePath: \vue-element-plus-temp\src\config\index.js
@@ -10,7 +10,7 @@
 
 const config = {}
 //配制请求URL
-config.req_url = process.env.NODE_ENV == 'development' ? 'http://1api.gzwea.com/' : 'http://api.gzwea.com/';
+config.req_url = process.env.NODE_ENV == 'development' ? 'http://1api.gzwea.com/' : 'https://api.vote.gzwea.com/';
 //配制上传文件时带的头信息
 config.upload_file_header = { "Content-Type": "multipart/form-data" };
 export default config;
