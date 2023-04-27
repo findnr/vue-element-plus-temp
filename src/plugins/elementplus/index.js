dcfd9728c6bb4b265dc851468caa16edf7d0c267
cym504875043 <504875043@qq.com>
2023-04-27 08:10:33 +0800

    2023-04-27 08:10:32

--- src/plugins/elementplus/index.js
+++ src/plugins/elementplus/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 程英明
  * @Date: 2022-01-14 13:45:11
- * @LastEditTime: 2022-03-01 16:44:11
+ * @LastEditTime: 2023-04-27 08:10:21
  * @LastEditors: 程英明
  * @Description: 
  * @FilePath: \vue-element-plus-temp\src\plugins\elementplus\index.js
@@ -13,11 +13,16 @@ import './middify.scss'
 
 import * as Icons from "@element-plus/icons-vue"
 import { ElMessage } from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 export function setupElementPlus(app) {
-    app.use(ElementPlus)
+    app.use(ElementPlus, {
+        locale: zhCn
+    })
     Object.keys(Icons).forEach((it) => {
         app.component(it, Icons[it]);
     });
+
     app.config.globalProperties.$message = ElMessage;
 }
+
