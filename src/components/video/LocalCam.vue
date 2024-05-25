988fb3801d3e6ee0944621060c4a5889b42974f4
findnr <cym504875043@gmail.com>
2024-05-25 10:39:43 +0800

    202405251039

--- src/components/video/LocalCam.vue
+++ src/components/video/LocalCam.vue
@@ -2,7 +2,7 @@
  * @Author: findnr
  * @Date: 2024-05-25 08:57:55
  * @LastEditors: findnr
- * @LastEditTime: 2024-05-25 09:02:52
+ * @LastEditTime: 2024-05-25 10:35:35
  * @Description: 
 -->
 <template>
@@ -12,21 +12,29 @@
 </template>
 
 <script setup>
-onMounted(()=>{
+ onMounted(async()=>{
     const video = document.getElementById('videoElement');
-    console.log(navigator.mediaDevices)
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true })
-        .then(function(stream) {
-            // 将视频流绑定到video元素上
-            video.srcObject = stream;
-        })
-        .catch(function(error) {
-            console.error('访问摄像头失败: ', error);
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const videoDevices = devices.filter(device => device.kind === 'videoinput');
+    console.log(videoDevices)
+    if (videoDevices.length > 0) {
+        // 假设虚拟摄像头是第一个视频设备
+        const virtualCamera = videoDevices[0];
+        // 请求访问虚拟摄像头
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: { 
+                deviceId: virtualCamera.deviceId , 
+                width: { ideal: 1920 },
+                height: { ideal: 1080 },
+            }
         });
+        // 将视频流绑定到video元素上
+        video.srcObject = stream;
     } else {
-        alert('你的浏览器不支持访问媒体设备。');
+        console.error('没有找到视频输入设备。');
     }
+    
+    
 })
 </script>
 
@@ -34,8 +42,8 @@ onMounted(()=>{
 
 .index{
     #videoElement {
-            width: 600px;
-            height: 400px;
+            width: 100%;
+            height: 100%;
             border: 1px solid black;
         }
 }
