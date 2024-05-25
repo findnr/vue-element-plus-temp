183db59de09ab7d2106c26889beefd8259b23966
findnr <cym504875043@gmail.com>
2024-05-25 09:04:49 +0800

    202405250904

---
+++ src/components/video/LocalCam.vue
@@ -0,0 +1,43 @@
+<!--
+ * @Author: findnr
+ * @Date: 2024-05-25 08:57:55
+ * @LastEditors: findnr
+ * @LastEditTime: 2024-05-25 09:02:52
+ * @Description: 
+-->
+<template>
+  <div class='index'>
+    <video id="videoElement" autoplay></video>
+  </div>
+</template>
+
+<script setup>
+onMounted(()=>{
+    const video = document.getElementById('videoElement');
+    console.log(navigator.mediaDevices)
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function(stream) {
+            // 将视频流绑定到video元素上
+            video.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error('访问摄像头失败: ', error);
+        });
+    } else {
+        alert('你的浏览器不支持访问媒体设备。');
+    }
+})
+</script>
+
+<style scoped lang="scss">
+
+.index{
+    #videoElement {
+            width: 600px;
+            height: 400px;
+            border: 1px solid black;
+        }
+}
+
+</style>
---
+++ src/views/index/home/cam.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='index'>
+    <h1>调用摄像头示例</h1>
+    
+    <LocalCam></LocalCam>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped lang="scss">
+
+.index{}
+
+</style>
